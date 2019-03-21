645785c3a489c6be82ff07690d519f773897c0f0
steelworx <eileencuevas62@gmail.com>
2019-03-20 21:36:59 -0400

    adds move_to function to class

--- src/player.py
+++ src/player.py
@@ -6,3 +6,18 @@ class Player:
     def __init__(self, name, location):
         self.name = name
         self.location = location
+
+    def move_to(self, direction):
+        if direction in ["n", "s", "e", "w"]:
+            if direction == "n" and hasattr(self.location, 'n_to'):
+                self.location = self.location.n_to
+            elif direction == "s" and hasattr(self.location, 's_to'):
+                self.location = self.location.s_to
+            elif direction == "e" and hasattr(self.location, 'e_to'):
+                self.location = self.location.e_to
+            elif direction == "w" and hasattr(self.location, 'w_to'):
+                self.location = self.location.w_to
+            else:
+                print(f"{self.name} walks into a wall! Try another direction.")
+        else:
+            print('Not a valid direction! Try n, s, e, or w!')
