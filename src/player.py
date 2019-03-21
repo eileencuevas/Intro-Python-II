9e0ec738fc2ec74eacae8e86289e9772b0e247f1
steelworx <eileencuevas62@gmail.com>
2019-03-21 16:15:31 -0400

    adds acquire_item & leave_item functions to player

--- src/player.py
+++ src/player.py
@@ -12,11 +12,11 @@ class Player:
             self.items = []
 
     def check_inventory(self):
-        if len(self.items) == 0:
-            print(f"No items in inventory!")
-        else:
+        if len(self.items) > 0:
             for item in self.items:
                 print(f"\n{item.name}: {item.description}")
+        else:
+            print(f"No items in inventory!")
 
     def move_to(self, direction):
         if direction in ["n", "s", "e", "w"]:
@@ -32,3 +32,21 @@ class Player:
                 print(f"\n{self.name} walks into a wall! Try another direction.")
         else:
             print('\nNot a valid direction! Try n, s, e, or w!')
+
+    def acquire_item(self, item_to_acquire):
+        for room_item in self.location.items:
+            # uses lower() to account for input variations
+            if room_item.name.lower() == item_to_acquire.lower():
+                self.location.remove_item(room_item)
+                self.items.append(room_item)
+                return room_item.on_take(self.name, self.location.name)
+        # will only fire if for loop returns nothing
+        return print(f"This item does not exist in this room!")
+
+    def leave_item(self, item_to_leave):
+        for inventory_item in self.items:
+            if inventory_item.name.lower() == item_to_leave.lower():
+                self.items.remove(inventory_item)
+                self.location.add_item(inventory_item)
+                return inventory_item.on_drop(self.name, self.location.name)
+        return print(f"You don't have this item in your inventory!")
