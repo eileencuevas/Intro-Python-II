e2b05255d809291cd7bb64656177321f52d3af77
steelworx <eileencuevas62@gmail.com>
2019-03-20 22:15:32 -0400

    added items to outside room for testing

--- src/adv.py
+++ src/adv.py
@@ -1,11 +1,14 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons."),
+                     "North of you, the cave mount beckons.",
+                     [Item("Pan", "A handy frying pan. Also a handy drying pan!"),
+                      Item("Bread", "Looks moldy.")]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -51,17 +54,20 @@ room['treasure'].s_to = room['narrow']
 #
 # If the user enters "q", quit the game.
 
-name = input('What is your name?    ')
+name = input('What is your name?\n    ')
 player = Player(str(name), room["outside"])
 
 
 def action_handler(action):
-    if action == 'items':
-        print(f"\n{player.location.return_items_list()}")
-    elif action == 'inventory':
-        print(f"\n{player.check_inventory()}")
-    else:
-        player.move_to(action)
+    if len(action) > 1:  # support for two words
+        print('\nThis is a two word command! Support comming soon!')
+    else:  # action only has one word
+        if action[0] == 'items':
+            player.location.print_items()
+        elif action[0] == 'inventory':
+            print(f"\n{player.check_inventory()}")
+        else:
+            player.move_to(action[0])
 
 
 while True:
@@ -69,8 +75,8 @@ while True:
         f"\n---{player.name}'s progress---\nCurrent Location: {player.location.name}\n\n")
     player.location.print_description()
     print(f"\nRoom Items: {player.location.return_items_list()}")
-    action = input('What would you like to do?    ')
+    action = input('What would you like to do?\n    ')
     if action == 'q':
         break
     else:
-        action_handler(action)
+        action_handler(action.split())
