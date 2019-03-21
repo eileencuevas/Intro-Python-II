5d9be7c5ef391481ec2960ca101e3649aaccdad1
steelworx <eileencuevas62@gmail.com>
2019-03-21 16:14:35 -0400

    adds on_take and on_drop functions to Item class

--- src/item.py
+++ src/item.py
@@ -2,3 +2,11 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+    def on_take(self, player_name, room_name):
+        print(
+            f"{player_name} has taken {self.name} from its home in {room_name}! What a find!")
+
+    def on_drop(self, player_name, room_name):
+        print(
+            f"{player_name} has dropped {self.name} onto the floor of the {room_name}. Oh well.")
